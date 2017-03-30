bfe185c1517ec59843f0c70a326c8a55417149a4
BucketOfMilk <seanbw0518@gmail.com>
2017-03-30 11:11:25 +0100

    Moved error alerts to bottom of page
    The error boxes that come up have been moved to the bottom of the page,
    and the red has been made stronger to be more noticable.

--- static/js/messages.js
+++ static/js/messages.js
@@ -1,6 +1,6 @@
 var messages = {
     message: function(state, header, body) {
-        var m = $("<div></div>", {"class": "ui message"}),
+        var m = $("<div></div>", {"class": "ui errorbox"}),
             i = $("<i></i>", {"class": "close icon"}),
             h = $("<div></div>", {"class": "header"}),
             b = $("<p></p>");
@@ -17,7 +17,7 @@ var messages = {
             m.transition('fade');
         });
 
-        $("#messages").append(m);
+        $("#errorbox").append(m);
     },
 
     info: function(msg) {
@@ -28,9 +28,16 @@ var messages = {
     error: function(msg) {
         messages.message("negative", "Something went wrong!", msg);
         machine.pause = true;
+        popUpBox();
     },
 
     success: function(msg) {
         messages.message("positive", "Yay!", msg);
     }
 };
+
+function popUpBox(){
+    var popup = document.getElementById("errorbox");
+    popup.style.display = "block";
+    popup.scrollTop = popup.scrollHeight;
+}
